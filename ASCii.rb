9242b0edad259c5f0df5f3200467710abaee0bb3
Pln <sai.praneeth.pln@accenture.com>
2022-07-21 12:40:46 +0530

    Addign Ascii assignment

---
+++ ASCii.rb
@@ -0,0 +1,33 @@
+#This is a simple ASCii art logic using simple method with parameters and if condition authored by P.L.N Sai Praneeth
+
+puts "Enter the Charater with which you want to make an art"
+character = gets.chomp
+character1 = '|'
+puts "How many rows of #{character}'s do you want?"
+row_count = gets.chomp.to_i
+
+character_count = 1
+width = 75
+puts "What ASCii art do you want me to print?\n
+    1. ASCii Pyramid\n
+    2. ASCii Tree\n"
+art = gets.chomp.to_i
+
+def tree(row_count,character,character_count,width)
+    
+    row_count.times do
+	  puts (character * character_count).center(width)
+	  character_count += 2
+    end
+end
+
+if art == 1
+    tree(row_count,character,character_count,width)
+elsif art == 2
+    tree(row_count,character,character_count,width)
+    row_count.times do
+        puts character1.center(width)
+    end
+else
+    puts "We can print only Pyramid and a tree. Please choose the option either 1 or 2" 
+end
